67af4c87031740582f69f73718f00d032a30f48a
Christopher Ebron <cebron88@gmail.com>
2021-09-28 13:44:53 -0400

    Game finished

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 'use strict';
 
 
-const winner = document.querySelector('#winner').innerHTML
+const winner = document.querySelector('#winner')
 const img1 = document.querySelector('.img1')
 const img2 = document.querySelector('.img2')
+const random1 = Math.floor(Math.random()* 6)
+const random2 = Math.floor(Math.random()* 6)
 
 
 const diceImgs = [
@@ -15,8 +17,17 @@ const diceImgs = [
   'images/dice6.png',
  ]
 
-img1.src = diceImgs[Math.floor(Math.random()* 6)];
-img2.src = diceImgs[Math.floor(Math.random()* 6)];
+img1.src = diceImgs[random1];
+img2.src = diceImgs[random2];
+
+
+  if((random1 + 1) === (random2 + 1)){
+    winner.innerHTML = 'Tie'
+  }else if ((random1 + 1) > (random2 + 1)){
+    winner.innerHTML = 'Player 1 Wins'
+  } else {
+     winner.innerHTML = 'Player 2 Wins'
+  }
 
 
 
